df52894ee83f137b65a080e1b110f48923e6cb84
Tatiana Zhukauskene <104422006+TatZhuk@users.noreply.github.com>
2022-06-05 16:37:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-test
+import requests
+
+
+def test_get_response_status_code():
+    response = requests.get('https://api.github.com/repos/TatZhuk/Test-version')
+    assert response.status_code == 200
